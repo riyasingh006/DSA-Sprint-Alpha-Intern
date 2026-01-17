1b19be81da0fd921447bfc4d115b36ab747401fa
riyasingh006 <riyasingh79877@gmail.com>
2026-01-17 19:19:04 +0530

    Added DAY-10 Vector programs

---
+++ DAY-10 Vector/basic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> v;
+    // v.push_back(10);   //insert element into the vector 
+    //v.push_back(50);
+    // v.push_back(80);
+    cout<<v[0]<<endl;
+    cout<<v[1]<<endl;
+    cout<<v[2];
+
+}
+
+
---
+++ DAY-10 Vector/input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> v(4);
+    for(int i=0; i<4; i++){
+        cin>>v[i];
+    }
+    for(int i=0; i<4; i++){
+        cout<<v[i]<<endl;
+    
+}
+}
